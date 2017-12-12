22b7e807e1d9ea63a54e5a0e1b19debdef291658
Alexandre Michel <alexandre.michel08@gmail.com>
2017-12-12 20:16:18 +0100

    Initial commit

---
+++ app/src/main/java/fr/utt/if26/todoist/Principal.java
@@ -0,0 +1,13 @@
+package fr.utt.if26.todoist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+    }
+}
