b5873567c13212d24c6affb69b1006438c4d4b35
Alexandre Michel <alexandre.michel08@gmail.com>
2017-12-12 20:22:12 +0100

    Initial commit2

--- app/src/main/java/fr/utt/if26/todoist/Principal.java
+++ app/src/main/java/fr/utt/if26/todoist/Principal.java
@@ -2,9 +2,12 @@ package fr.utt.if26.todoist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Principal extends AppCompatActivity {
 
+    Button continuer;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
